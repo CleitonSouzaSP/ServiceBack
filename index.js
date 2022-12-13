09914db95ce3c0c268fb3bb1a020db578c3f3000
CleitonSouzaSP <suporte_bts@hotmail.com>
2022-12-13 18:12:27 -0300

    Ajuste Rota Login

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3333;
+
+app.use(bodyParser.json());
+
+app.get('/login', (req, res) => {
+    const body = req.body;
+
+    console.log('Usuario não encontrado!');
+    res.send('Usuário não encontrado!');
+
+})
+
+
+app.listen(port, () => console.log('Running on port: ' + port));
+
